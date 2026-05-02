6b14329eb173304d477dc721773ee62e746a3cb5
poiuyttt <50654486+poiuyttt@users.noreply.github.com>
2026-05-02 19:28:21 +0800

    feat: 添加登录页面和路由权限守卫

--- blog-frontend/src/router/index.ts
+++ blog-frontend/src/router/index.ts
@@ -2,9 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue";
 import AboutPage from "../views/AboutPage.vue";
 import SearchPage from "../views/SearchPage.vue";
-import ArticlePage from "../views/ArticlePage.vue";
-
-import { useRouter } from "vue-router";
+import LoginPage from "../views/LoginPage.vue";
 
 const routes = [
   {
@@ -18,9 +16,15 @@ const routes = [
     component: AboutPage,
   },
   {
+    path: "/login",
+    name: "Login",
+    component: LoginPage,
+  },
+  {
     path: "/article/:id",
     name: "article",
-    component: ArticlePage,
+    component: () => import("../views/ArticlePage.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/search",
@@ -32,6 +36,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  const isLoggedIn = !!localStorage.getItem("token");
+
+  if (requiresAuth && !isLoggedIn) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ blog-frontend/src/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const username = ref<string>("");
+function login(): void {
+  localStorage.setItem("token", "fake-jwt-token");
+  alert("登录成功");
+
+  router.push("/");
+}
+</script>
+<template>
+  <div style="text-align: center; padding: 50px">
+    <h2>登录页面</h2>
+    <input v-model="username" placeholder="请输入用户名" />
+    <button @click="login">登录</button>
+  </div>
+</template>
